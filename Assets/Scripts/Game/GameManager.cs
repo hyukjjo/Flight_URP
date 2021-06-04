9f8dd08f3987b645a4623210fee986c4710cb381
hadpp <ygpoket@naver.com>
2021-06-04 21:37:43 +0900

    게임 씬 레이어 관리, 테스트 게임씬과 합침

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -8,7 +8,8 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
     }
 
     // Start is called before the first frame update
