4eba2d98278fd1ca3882b21448b9a9f3f5de1803
hyukjjo <hyukjjo@gmail.com>
2021-07-22 09:02:18 +0900

    Main scene

--- Assets/Scripts/Main/UIViewGold.cs
+++ Assets/Scripts/Main/UIViewGold.cs
@@ -2,20 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System;
 
-public class UIViewMain : UIViewBase
+public class UIViewGold : UIViewBase
 {
     public Text textPlayerGold;
 
     public override void InitView()
     {
-        MainManager.SetMainState(EMainState.MAIN);
-        Timer.InitTimer();
+        textPlayerGold.text = DataManager.Instance.GetPlayerData().playerGold.ToString();
     }
 
     public override void ResetView()
     {
-        
+
     }
 }
