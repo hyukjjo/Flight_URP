a01fa4a3aefa9c032859a6d8ed907395aa35c08e
박경권 <pkk@skonec.com>
2022-05-02 12:11:14 +0900

    FU-00 리팩토링 연습 test: 설계 및 실패 테스트 작성

---
+++ Assets/Scripts_Tests/EditModeTests/Player_Movement.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class Player_Movement
+{
+    [Test, Description("플레이어는 InputFactory를 통해 PlayerInput을 주입 받을 수 있다.")]
+    public void player_can_be_injected_with_PlayerInput_via_InputFactory()
+    {
+        IPlayerInput playerInput = InputFactory.GetPlayerInput();
+
+        Assert.IsTrue(playerInput != null);
+    }
+}
+
+// Stub
+public class InputFactory
+{
+    public static IPlayerInput GetPlayerInput()
+    {
+        return null;
+    }
+}
