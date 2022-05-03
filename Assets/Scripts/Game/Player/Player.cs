7770edc62b09ad3f54cb0a320f65c92808e6f8c6
박경권 <pkk@skonec.com>
2022-05-03 18:20:36 +0900

    FU-00 리팩토링 연습 refac: 클래스 추출

--- Assets/Scripts/Game/Player/Player.cs
+++ Assets/Scripts/Game/Player/Player.cs
@@ -2,44 +2,125 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Figure
+public class PlayerMoveCtrl
 {
-    [Header("----------Player----------")]
-    public bool isTouchable = false;
-    //public bool isMoving = false;
-
-    private Vector2 touchPos = Vector2.zero;
-    // 응집도 낮음 - "몇몇 메서드만이 사용하는 인스턴스 변수", 클린코드 177p
+    private const float LEFT_SIDE = -1.0f;
+    private const float RIGHT_SIDE = 1.0f;
+    private Player _player;
     private float screenCenterX = Screen.width * 0.5f;
     private float moveTime = 0.1f;
     private Coroutine corMove = null;
     private Vector2 nextPos = Vector2.zero;
     private List<GameObject> enemys = new List<GameObject>();
+    private float offset = 1.5f;
+    private float _prevInputDownX;
 
+    public PlayerMoveCtrl(Player player)
+    {
+        _player = player;
+    }
+
+    public void ApplyInput(IPlayerInput playerInput)
+    {
+        _prevInputDownX = playerInput.GetInputPositionXDown();
+        MoveByTouch();
+        MoveBySwipe();
+    }
+
+    private void MoveByTouch()
+    {
+        corMove = _player.StartCoroutine(TryMovePlayer(CheckTouchLeftOrRight(_prevInputDownX)));
+    }
+
+    private float CheckTouchLeftOrRight(float inputPositionX) => inputPositionX - screenCenterX >= 0.0 ? RIGHT_SIDE : LEFT_SIDE;
+
+    private void MoveBySwipe()
+    {
+        corMove = _player.StartCoroutine(TryMovePlayer(CheckSwipeLeftOrRight(CalcDiffInputDownAndUp())));
+    }
+
+    private float CalcDiffInputDownAndUp() => _player.PlayerInput.GetInputPositionXUp() - _prevInputDownX;
+    private float CheckSwipeLeftOrRight(float diffInputDownAndUp) => diffInputDownAndUp > 0.0f ? RIGHT_SIDE : LEFT_SIDE;
+
+    private IEnumerator TryMovePlayer(float directionX)
+    {
+        if (NeedRetryMovePlayer(directionX)) yield break;
+
+        Vector2 startPos = _player.transform.position;
+        nextPos = startPos + new Vector2(directionX * offset, 0f);
+
+        if (!CanMoveTo(nextPos))
+        {
+            corMove = null;
+            nextPos = Vector2.zero;
+            yield break;
+        }
+        
+        yield return _player.StartCoroutine(ProceedMove(startPos));
+    }
+
+    private IEnumerator ProceedMove(Vector2 startPos)
+    {
+        float timer = 0f;
+        while (CheckTimer(timer))
+        {
+            timer += Time.deltaTime;
+            float elapsedTimeRate = timer / moveTime;
+            _player.transform.position = Vector2.Lerp(startPos, nextPos, elapsedTimeRate);
+            yield return null;
+        }
+
+        _player.transform.position = nextPos;
+    }
+
+    private bool NeedRetryMovePlayer(float directionX)
+    {
+        if (!IsBeingMoved()) return false;
+        _player.StopAllCoroutines();
+        corMove = _player.StartCoroutine(TryMovePlayer(directionX));
+        return true;
+    }
+
+    private bool IsBeingMoved() => corMove != null;
+    private bool CheckTimer(float timer) => timer < moveTime;
+
+    private bool CanMoveTo(Vector2 nextPos)
+    {
+        enemys = GameManager.Instance.enemyManager.enemys;
+        if (IsEnemyEmpty()) return false;
+        return !IsOutsideOfValidArea(nextPos);
+    }
+
+    private bool IsOutsideOfValidArea(Vector2 nextPos) => IsGoThroughLeftFence(nextPos) || IsGoThroughRightFence(nextPos);
+    private bool IsGoThroughRightFence(Vector2 nextPos) => nextPos.x > enemys[enemys.Count - 1].transform.position.x;
+    private bool IsGoThroughLeftFence(Vector2 nextPos) => enemys[0].transform.position.x > nextPos.x;
+    private bool IsEnemyEmpty() => enemys.Count <= 0;
+}
+
+public class Player : Figure
+{
+    [Header("----------Player----------")]
+    public bool isTouchable = false;
     public SpriteRenderer facial;
 
     private float oddX = 0f;
     private float evenX = -0.75f;
-    private float offset = 1.5f;
-    // 응집도 낮음 - "몇몇 메서드만이 사용하는 인스턴스 변수", 클린코드 177p
-    private float _prevInputDownX;
+    private PlayerMoveCtrl _playerMoveCtrl;
+
     public IPlayerInput PlayerInput { get; set; }
-    private const float LEFT_SIDE = -1.0f;
-    private const float RIGHT_SIDE = 1.0f;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
             Init();
         if (!isTouchable) return;
-        _prevInputDownX = PlayerInput.GetInputPositionXDown();
-        MoveByTouch();
-        MoveBySwipe();
+        _playerMoveCtrl.ApplyInput(PlayerInput);
     }
 
     public override void Init()
     {
         base.Init();
-
+        _playerMoveCtrl = new PlayerMoveCtrl(this);
         SetPosition();
         figure = FigureData.GetRandomData(GameManager.Instance.EnemyCount);
         SetFigure(figure);
@@ -58,78 +139,6 @@ public class Player : Figure
 
         model.sprite = GameManager.Instance.models[(int)figure.shape];
     }
-
-    public void MoveByTouch()
-    {
-        corMove = StartCoroutine(TryMovePlayer(CheckTouchLeftOrRight(_prevInputDownX)));
-    }
-
-    private float CheckTouchLeftOrRight(float inputPositionX) => inputPositionX - screenCenterX >= 0.0 ? RIGHT_SIDE : LEFT_SIDE;
-
-    public void MoveBySwipe()
-    {
-        corMove = StartCoroutine(TryMovePlayer(CheckSwipeLeftOrRight(CalcDiffInputDownAndUp())));
-    }
-
-    private float CalcDiffInputDownAndUp() => PlayerInput.GetInputPositionXUp() - _prevInputDownX;
-
-    private float CheckSwipeLeftOrRight(float diffInputDownAndUp) => diffInputDownAndUp > 0.0f ? RIGHT_SIDE : LEFT_SIDE;
-    
-    private IEnumerator TryMovePlayer(float directionX)
-    {
-        if (NeedRetryMovePlayer(directionX)) yield break;
-
-        Vector2 startPos = transform.position;
-        nextPos = startPos + new Vector2(directionX * offset, 0f);
-
-        if (!CanMoveTo(nextPos))
-        {
-            corMove = null;
-            nextPos = Vector2.zero;
-            yield break;
-        }
-        
-        yield return StartCoroutine(ProceedMove(startPos));
-    }
-
-    private IEnumerator ProceedMove(Vector2 startPos)
-    {
-        float timer = 0f;
-        while (CheckTimer(timer))
-        {
-            timer += Time.deltaTime;
-            float elapsedTimeRate = timer / moveTime;
-            transform.position = Vector2.Lerp(startPos, nextPos, elapsedTimeRate);
-            yield return null;
-        }
-        transform.position = nextPos;
-    }
-
-    private bool NeedRetryMovePlayer(float directionX)
-    {
-        if (!IsBeingMoved()) return false;
-        StopAllCoroutines();
-        corMove = StartCoroutine(TryMovePlayer(directionX));
-        return true;
-    }
-
-    private bool IsBeingMoved() => corMove != null;
-
-    private bool CheckTimer(float timer) => timer < moveTime;
-
-    private bool CanMoveTo(Vector2 nextPos)
-    {
-        enemys = GameManager.Instance.enemyManager.enemys;
-        if (IsEnemyEmpty()) return false;
-        return !IsOutsideOfValidArea(nextPos);
-    }
-    private bool IsOutsideOfValidArea(Vector2 nextPos) => IsGoThroughLeftFence(nextPos) || IsGoThroughRightFence(nextPos);
-
-    private bool IsGoThroughRightFence(Vector2 nextPos) => nextPos.x > enemys[enemys.Count - 1].transform.position.x;
-
-    private bool IsGoThroughLeftFence(Vector2 nextPos) => enemys[0].transform.position.x > nextPos.x;
-
-    private bool IsEnemyEmpty() => enemys.Count <= 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
